cd36b8a684386d524d2a443abbd119faaee43828
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-18 22:05:54 +0700

    Election.Analytic.System - PPRP  #16 - Update projects   - Add Sample projects     - Add EPPlus.Wpf project.

---
+++ 20.Samples/EPPlus.Samples/EPPlus.Wpf/App.xaml.cs
@@ -0,0 +1,134 @@
+﻿#define SINGELTON_APP
+
+#region Using
+
+using System;
+using System.Windows;
+
+using NLib;
+using NLib.Logs;
+
+
+#endregion
+
+namespace EPPlus.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// OnStartup.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Console.WriteLine("OnStartUp");
+            if (null != AppDomain.CurrentDomain)
+            {
+                if (null != System.Threading.Thread.CurrentContext)
+                {
+                    Console.WriteLine("Thread CurrentContext is not null.");
+                }
+            }
+
+            #region Create Application Environment Options
+
+            EnvironmentOptions option = new EnvironmentOptions()
+            {
+                /* Setup Application Information */
+                AppInfo = new NAppInformation()
+                {
+                    /*  This property is required */
+                    CompanyName = "Softbase",
+                    /*  This property is required */
+                    ProductName = "EPPlus Sample",
+                    /* For Application Version */
+                    Version = "1",
+                    Minor = "0",
+                    Build = "0",
+                    LastUpdate = new DateTime(2022, 7, 14, 20, 20, 00)
+                },
+                /* Setup Storage */
+                Storage = new NAppStorage()
+                {
+                    StorageType = NAppFolder.Application
+                },
+                /* Setup Behaviors */
+                Behaviors = new NAppBehaviors()
+                {
+                    //***********************************************************************************
+                    // NOTE:
+                    //***********************************************************************************
+                    // WHEN Use in same PC required to change port number otherwiser app will die.
+                    //***********************************************************************************
+#if SINGELTON_APP
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = true,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+#else
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = false,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+#endif
+                }
+            };
+
+            #endregion
+
+            #region Setup Option to Controller and check instance
+
+            WpfAppContoller.Instance.Setup(option);
+
+            if (option.Behaviors.IsSingleAppInstance &&
+                WpfAppContoller.Instance.HasMoreInstance)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Init Preload classes
+
+            ApplicationManager.Instance.Preload(() =>
+            {
+
+            });
+
+            #endregion
+
+            // Start log manager
+            LogManager.Instance.Start();
+
+            Window window = null;
+            window = new MainWindow();
+
+            if (null != window)
+            {
+                WpfAppContoller.Instance.Run(window);
+            }
+        }
+        /// <summary>
+        /// OnExit
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Shutdown log manager
+            LogManager.Instance.Shutdown();
+
+            // Wpf shutdown process required exit code.
+
+            /* If auto close the single instance must be true */
+            bool autoCloseProcess = true;
+            WpfAppContoller.Instance.Shutdown(autoCloseProcess, e.ApplicationExitCode);
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ 20.Samples/EPPlus.Samples/EPPlus.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+using OfficeOpenXml;
+using EPPlus;
+using EPPlus.DataExtractor;
+
+#endregion
+
+namespace EPPlus.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // set license.
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var xlsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Examples");
+            string fileName = string.Empty;
+
+            OpenFileDialog fd = new OpenFileDialog();
+            //fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            fd.InitialDirectory = xlsPath;
+            fd.Multiselect = false;
+            fd.Filter = "Excel file (*.xls, *.xlsx)|*.xls;*.xlsx";
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = fd.FileName;
+            }
+            fd.Dispose();
+            fd = null;
+            if (string.IsNullOrWhiteSpace(fileName)) return;
+
+            txtFileName.Text = fileName;
+            LoadWorksheets(fileName);
+        }
+
+        #endregion
+
+        #region ListView Handlers
+
+        private void lvSheets_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string fileName = txtFileName.Text;
+            var item = lvSheets.SelectedItem as Sheet;
+            if (null == item) return;
+
+            LoadSheetData(fileName, item.Name);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void LoadWorksheets(string fileName)
+        {
+            lvSheets.ItemsSource = null;
+
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return;
+            }
+
+            List<Sheet> sheets = new List<Sheet>();
+            using (var package = new ExcelPackage(fileName))
+            {
+                var worksheets = package.Workbook.Worksheets;
+                foreach (var worksheet in worksheets)
+                {
+                    sheets.Add(new Sheet() { Name = worksheet.Name });
+                }
+            }
+
+            lvSheets.ItemsSource = sheets;
+        }
+
+
+        private void LoadSheetData(string fileName, string sheetName)
+        {
+            gridSheetData.ItemsSource = null;
+
+            using (var package = new ExcelPackage(fileName))
+            {
+                var sheet = package.Workbook.Worksheets[sheetName];
+                if (null != sheet)
+                {
+                    var accessor = sheet.Extract<GeoLocation>()
+                        // Here we can chain multiple definition for the columns
+                        .WithProperty(p => p.ProvinceCode, "A")
+                        .WithProperty(p => p.AmphurCode, "B")
+                        .WithProperty(p => p.TumbonCode, "C")
+                        .WithProperty(p => p.ProvinceName, "E")
+                        .WithProperty(p => p.AmphurName, "F")
+                        .WithProperty(p => p.TumbonName, "G");
+                    var results = accessor
+                        .GetData(3, row => null != sheet.Cells[row, 1].Value).ToList();
+                    gridSheetData.ItemsSource = results;
+                }
+            }
+        }
+
+        #endregion
+
+        public class Sheet
+        {
+            public string Name { get; set; }
+        }
+
+        public class GeoLocation
+        {
+            public string ProvinceCode { get; set; }
+            public string AmphurCode { get; set; }
+
+            public string TumbonCode { get; set; }
+
+            public string ProvinceName { get; set; }
+
+            public string AmphurName { get; set; }
+
+            public string TumbonName { get; set; }
+        }
+    }
+}
