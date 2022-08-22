560484eaa01c76b81ecf5a78b0366c5201099cb2
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-22 10:23:22 +0700

    Election.Analytic.System - PPRP  #63 - Update projects   - Update PPRP.Controls project.     - Add TemplatePage.xaml -> used for reference template all pages.     - Implements SignInPage #1.

---
+++ 05.Controls/PPRP.Controls/Pages/Templates/TemplatePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPRP.Pages.Templates
+{
+    /// <summary>
+    /// Interaction logic for TemplatePage.xaml
+    /// </summary>
+    public partial class TemplatePage : UserControl
+    {
+        public TemplatePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
