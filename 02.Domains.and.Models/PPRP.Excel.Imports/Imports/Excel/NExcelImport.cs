ef4d0b464e5e90e909c8c97a4e1ff8ab90754d20
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-26 18:31:57 +0700

    Election.Analytic.System - PPRP  #103 - Update projects   - PPRP.Excel.Imports.     - Implements Excel import model and processes #2.

--- 02.Domains.and.Models/PPRP.Excel.Imports/Imports/Excel/NExcelImport.cs
+++ 02.Domains.and.Models/PPRP.Excel.Imports/Imports/Excel/NExcelImport.cs
@@ -16,7 +16,87 @@ using EPPlus.DataExtractor;
 
 namespace PPRP.Imports.Excel
 {
-    public class NExcelImport
+    #region NExcelImport
+
+    /// <summary>
+    /// The NExcelImport class.
+    /// </summary>
+    public class NExcelImport : IDisposable
     {
+        #region Internal Variables
+
+        private bool disposedValue;
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NExcelImport() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~NExcelImport()
+        {
+            // Do not change this code.
+            // Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region protected Methods
+
+        /// <summary>
+        /// Dispose.
+        /// </summary>
+        /// <param name="disposing">True when in disposing process.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose managed state (managed objects)
+                }
+                // Free unmanaged resources (unmanaged objects) and override finalizer
+
+                // Set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Dispose.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string FileName { get; set; }
+
+        #endregion
     }
+
+    #endregion
 }
