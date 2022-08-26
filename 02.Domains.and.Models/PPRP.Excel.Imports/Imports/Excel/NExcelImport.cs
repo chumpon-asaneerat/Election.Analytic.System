868c5031ea7a72f4b3f5bdfa636b7917f59a00a2
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-26 18:26:13 +0700

    Election.Analytic.System - PPRP  #102 - Update projects   - PPRP.Excel.Imports.     - Implements Excel import model and processes.

---
+++ 02.Domains.and.Models/PPRP.Excel.Imports/Imports/Excel/NExcelImport.cs
@@ -0,0 +1,22 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+using OfficeOpenXml;
+using EPPlus;
+using EPPlus.DataExtractor;
+
+#endregion
+
+namespace PPRP.Imports.Excel
+{
+    public class NExcelImport
+    {
+    }
+}
