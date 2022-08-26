a495b30f5d4231e4efb70c40e0eeec3217ae93ee
chumpon-asaneerat <chumponsenate@gmail.com>
2022-08-26 18:58:00 +0700

    Election.Analytic.System - PPRP  #106 - Update projects   - PPRP.Excel.Imports, PPRP.Manangement.App.     - Implements Excel import model and processes #5.

---
+++ 09.App/PPRP.Manangement.App/Pages/Samples/ExcelImportSamplePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+#endregion
+
+namespace PPRP.Pages
+{
+    /// <summary>
+    /// Interaction logic for ExcelImportSamplePage.xaml
+    /// </summary>
+    public partial class ExcelImportSamplePage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ExcelImportSamplePage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+        }
+
+        #endregion
+    }
+}
