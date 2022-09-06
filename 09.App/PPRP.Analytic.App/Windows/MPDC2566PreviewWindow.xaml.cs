fd5efaf847f68cc8ffe4f310d44f1c31ca85078c
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 02:41:23 +0700

    Election.Analytic.System - PPRP  #233 - Update projects   - Update PPRP.Analytic.App project     - Implements MPDC2566PreviewWindow #1.

---
+++ 09.App/PPRP.Analytic.App/Windows/MPDC2566PreviewWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Reflection;
+
+using NLib;
+using NLib.Services;
+
+using PPRP.Domains;
+
+#endregion
+
+namespace PPRP.Windows
+{
+    /// <summary>
+    /// Interaction logic for MPDC2566PreviewWindow.xaml
+    /// </summary>
+    public partial class MPDC2566PreviewWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MPDC2566PreviewWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private MPDC2566Summary _person = null;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup(MPDC2566Summary person)
+        {
+            _person = person;
+            if (null != _person)
+            {
+                
+            }
+        }
+
+        #endregion
+    }
+}
