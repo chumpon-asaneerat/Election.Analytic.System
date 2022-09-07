8ba5394798d94097b18a933de4ed5bc89e3d204a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 11:51:25 +0700

    Election.Analytic.System - PPRP  #254 - Update projects   - Update PPRP.Analytic.App, PPRP.Manangement.App project     - Implements MPD2562PollingUnitSummary #1.

---
+++ 02.Domains.and.Models/PPRP.Domains/Domains/MPD2562PollingUnitSummary.cs
@@ -0,0 +1,126 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace PPRP.Domains
+{
+    #region MPD2562PollingUnitSummary
+
+    public class MPD2562PollingUnitSummary
+    {
+        #region Public Properties
+
+        public string ProvinceName { get; set; }
+        public int PollingUnitNo { get; set; }
+        public int PollingUnitCount { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static NDbResult<List<MPD2562PollingUnitSummary>> Gets()
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<MPD2562PollingUnitSummary>> rets = new NDbResult<List<MPD2562PollingUnitSummary>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            try
+            {
+                string query = string.Empty;
+                query += @"
+                    SELECT * 
+                      FROM MPD2562PollingUnitSummary
+                ";
+
+                rets.Value = cnn.Query<MPD2562PollingUnitSummary>(query).ToList();
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            if (null == rets.Value)
+            {
+                // create empty list.
+                rets.Value = new List<MPD2562PollingUnitSummary>();
+            }
+
+            return rets;
+        }
+
+        public static void Save(MPD2562PollingUnitSummary value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+
+                return;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ProvinceName", value.ProvinceName);
+            p.Add("@PollingUnitNo", value.PollingUnitNo);
+            p.Add("@PollingUnitCount", value.PollingUnitCount);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveMPD2562PollingUnitSummary", p, commandType: CommandType.StoredProcedure);
+
+                // Set error number/message
+                int errNum = p.Get<int>("@errNum");
+                string errMsg = p.Get<string>("@errMsg");
+                if (errNum != 0)
+                {
+                    Console.WriteLine(errMsg);
+                }
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+            }
+
+            return;
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
