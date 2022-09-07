b0e100300bbb3b850e2d7a607190a921f273cb77
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-07 11:59:22 +0700

    Election.Analytic.System - PPRP  #256 - Update projects   - Update PPRP.Analytic.App, PPRP.Manangement.App project     - Implements MPD2562PollingUnitSummary #3.

---
+++ 09.App/PPRP.Manangement.App/Pages/MPD/MPD2562PollingUnitSummaryManagePage.xaml.cs
@@ -0,0 +1,92 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Services;
+
+using PPRP.Domains;
+
+#endregion
+
+namespace PPRP.Pages
+{
+    /// <summary>
+    /// Interaction logic for MPD2562PollingUnitSummaryManagePage.xaml
+    /// </summary>
+    public partial class MPD2562PollingUnitSummaryManagePage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MPD2562PollingUnitSummaryManagePage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenuPage();
+        }
+
+        private void cmdImport_Click(object sender, RoutedEventArgs e)
+        {
+            Import();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GotoMainMenuPage()
+        {
+            var page = PPRPApp.Pages.MainMenu;
+            page.Setup();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void Import()
+        {
+            /*
+            var win = PPRPApp.Windows.ImportMPD2562VoteSummary;
+            win.Setup();
+            if (win.ShowDialog() == false)
+            {
+                return;
+            }
+            RefreshList();
+            */
+        }
+
+        private void RefreshList()
+        {
+            /*
+            lvMPD2562Summaries.ItemsSource = null;
+            var summaries = MPD2562VoteSummary.Gets();
+            lvMPD2562Summaries.ItemsSource = (null != summaries) ? summaries.Value : new List<MPD2562VoteSummary>();
+            */
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            RefreshList();
+        }
+
+        #endregion
+    }
+}
