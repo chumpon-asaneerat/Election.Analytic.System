c508186a5628a12e930e9c4c5084a101e43b274e
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-09 13:02:37 +0700

    Election.Analytic.System - PPRP  #285 - Update projects   - Implements Wpf.Canvas.Sample project #12.

--- 20.Samples/Wpf.Canvas/Wpf.Canvas.Sample/NResourceMonitor.cs
+++ 20.Samples/Wpf.Canvas/Wpf.Canvas.Sample/NResourceMonitor.cs
@@ -45,7 +45,7 @@ namespace NLib.Services
     {
         #region Singelton Access
 
-        public static NResourceMonitor _instance = null;
+        private static NResourceMonitor _instance = null;
 
         /// <summary>
         /// Singelton Access Instance.
@@ -70,9 +70,16 @@ namespace NLib.Services
         #region Internal Variables
 
         private Thread _th = null;
+        private bool _isExit = false;
         private bool _running = false;
+
+        private int _RefreshInSeconds = 1;
         private DateTime _lastUpdate = DateTime.MinValue;
+
         private NResourceUsage _usage = new NResourceUsage();
+
+        private static PerformanceCounter _cpu = null;
+        private static PerformanceCounter _ram = null;
 
         #endregion
 
@@ -81,12 +88,31 @@ namespace NLib.Services
         /// <summary>
         /// Constructor.
         /// </summary>
-        private NResourceMonitor() : base() { }
+        private NResourceMonitor() : base() 
+        {
+            _isExit = false;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+        }
         /// <summary>
         /// Destructor.
         /// </summary>
         ~NResourceMonitor()
         {
+            if (null != _cpu)
+            {
+                try { _cpu.Dispose(); }
+                catch { }
+            }
+            _cpu = null;
+
+            if (null != _ram)
+            {
+                try { _ram.Dispose(); }
+                catch { }
+            }
+            _ram = null;
+
+            AppDomain.CurrentDomain.ProcessExit -= CurrentDomain_ProcessExit;
             Shutdown();
         }
 
@@ -94,22 +120,105 @@ namespace NLib.Services
 
         #region Private Methods
 
+        #region App Domain Handlers
+
+        private void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            _isExit = true; // application is exit.
+        }
+
+        #endregion
+
+        #region Process/PerformanceCounters Methods
+
+        private string GetCurrentProcessName()
+        {
+            string name = string.Empty;
+
+            var process = Process.GetCurrentProcess();
+            process = Process.GetCurrentProcess();
+
+            // Preparing variable for application instance name
+            foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+            {
+                if (instance.StartsWith(process.ProcessName))
+                {
+                    using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
+                    {
+                        if (process.Id == (int)processId.RawValue)
+                        {
+                            name = instance;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return name;
+        }
+
+        private void CreatePerformanceCounters()
+        {
+            if (null == _cpu || null == _ram)
+            {
+                string processName = GetCurrentProcessName();
+                if (null == _cpu && !string.IsNullOrWhiteSpace(processName.Trim()))
+                {
+                    _cpu = new PerformanceCounter("Process", "% Processor Time", processName, true);
+                }
+                if (null == _ram && !string.IsNullOrWhiteSpace(processName.Trim()))
+                {
+                    _ram = new PerformanceCounter("Process", "Private Bytes", processName, true);
+                }
+            }
+            if (null == _cpu || null == _ram)
+                return;
+            // Getting first initial values
+            _cpu.NextValue();
+            _ram.NextValue();
+        }
+
+        private void UpdateResourceInfo()
+        {
+            if (null == _cpu || null == _ram)
+            {
+                CreatePerformanceCounters();
+            }
+            // recheck
+            if (null == _cpu || null == _ram)
+                return;
+            // Creating delay to get correct values of CPU usage during next query
+            Thread.Sleep(500);
+
+            // check instance.
+            if (null == _usage) _usage = new NResourceUsage();
+
+            lock (_usage)
+            {
+                // If system has multiple cores, that should be taken into account
+                _usage.CPU = Math.Round(_cpu.NextValue() / Environment.ProcessorCount, 2);
+                // Returns number of MB consumed by application
+                _usage.RAM = Math.Round(_ram.NextValue() / 1024 / 1024, 2);
+            }
+        }
+
+        #endregion
+
+        #region Thread Method
+
         private void Processing()
         {
-            while (_running && null != _th)
+            while (_running && null != _th && !_isExit)
             {
                 var ts = DateTime.Now - _lastUpdate;
-                if (ts.TotalSeconds >= 1)
+                if (ts.TotalSeconds >= _RefreshInSeconds)
                 {
                     UpdateResourceInfo();
                 }
             }
         }
 
-        private void UpdateResourceInfo()
-        {
-
-        }
+        #endregion
 
         #endregion
 
@@ -165,6 +274,24 @@ namespace NLib.Services
         public bool IsRunning
         {
             get { return (_running && null != _th); }
+        }
+        /// <summary>
+        /// Gets or sets refresh per seconds.
+        /// </summary>
+        public int RefreshInSeconds
+        {
+            get { return _RefreshInSeconds; }
+            set
+            {
+                if (_RefreshInSeconds != value)
+                {
+                    lock (this)
+                    {
+                        _RefreshInSeconds = value;
+                        if (_RefreshInSeconds < 1) _RefreshInSeconds = 1;
+                    }
+                }
+            }
         }
 
         #endregion
