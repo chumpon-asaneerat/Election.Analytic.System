4f56adf70c5153dbf30027cbbc0cc6582b27093b
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-11 18:35:58 +0700

    Election.Analytic.System - PPRP  #305 - Update projects   - Update PPRP.Domains project     - Implements MProvince class #3.

---
+++ 09.App/PPRP.Manangement.App/Pages/GeoLocation/MProvinceManagePage.xaml.cs
@@ -0,0 +1,89 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Services;
+
+using PPRP.Domains;
+
+#endregion
+
+namespace PPRP.Pages
+{
+    /// <summary>
+    /// Interaction logic for MProvinceManagePage.xaml
+    /// </summary>
+    public partial class MProvinceManagePage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MProvinceManagePage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenuPage();
+        }
+
+        private void cmdImport_Click(object sender, RoutedEventArgs e)
+        {
+            Import();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GotoMainMenuPage()
+        {
+            var page = PPRPApp.Pages.MainMenu;
+            page.Setup();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void Import()
+        {
+            /*
+            var win = PPRPApp.Windows.ImportPollingStation;
+            win.Setup();
+            if (win.ShowDialog() == false)
+            {
+                return;
+            }
+            */
+        }
+
+        private void RefreshList()
+        {
+            lvGeoLocations.ItemsSource = null;
+            var locations = PollingStation.Gets();
+            lvGeoLocations.ItemsSource = (null != locations) ? locations.Value : new List<PollingStation>();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            RefreshList();
+        }
+
+        #endregion
+    }
+}
