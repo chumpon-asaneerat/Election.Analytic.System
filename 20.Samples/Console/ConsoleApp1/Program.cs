f38eba4bdd5ec633951d54d71318e41ead412f00
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-12 19:05:38 +0700

    Election.Analytic.System - PPRP  #331 - Update projects   - Update ConsoleApp1 project #2.

---
+++ 20.Samples/Console/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+
+
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+            Console.WriteLine("Finished");
+        }
+    }
+}
