a34248cbb885853020ddbbd461637e6794d393e0
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-13 15:03:25 +0700

    Election.Analytic.System - PPRP  #344 - Update projects   - Implements MPD2562PollingUnitSummary ImportAreaRemark #6.

---
+++ 09.App/PPRP.Analytic.App/Windows/MPDC2562AreaRemarkWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Reflection;
+
+using NLib;
+using NLib.Services;
+
+using PPRP.Domains;
+
+#endregion
+
+namespace PPRP.Windows
+{
+    /// <summary>
+    /// Interaction logic for MPDC2562AreaRemarkWindow.xaml
+    /// </summary>
+    public partial class MPDC2562AreaRemarkWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MPDC2562AreaRemarkWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private MPD2562PollingUnitSummary _item = null;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup(MPD2562PollingUnitSummary value)
+        {
+            _item = value;
+            this.DataContext = _item;
+            if (null == _item)
+            {
+                txtTitle.Text = string.Format("ข้อมูลพื้นที่ {0} เขต {1}", "-", "-");
+            }
+            else
+            {
+                txtTitle.Text = string.Format("ข้อมูลพื้นที่ {0} เขต {1}", _item.ProvinceName, _item.PollingUnitNo);
+            }
+            this.Title = txtTitle.Text;
+        }
+
+        #endregion
+    }
+}
