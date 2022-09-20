6c58a28bdf86301fa834b56fa98c406a2f42f302
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-20 21:26:02 +0700

    Election.Analytic.System - PPRP  #510 - Update App version.

--- 01.Core/PPRP.Core/Consts/AppConsts.cs
+++ 01.Core/PPRP.Core/Consts/AppConsts.cs
@@ -20,8 +20,8 @@ namespace PPRP
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "500";
-                public static DateTime LastUpdate = new DateTime(2022, 09, 20, 18, 30, 00);
+                public static string Build = "510";
+                public static DateTime LastUpdate = new DateTime(2022, 09, 20, 20, 30, 00);
             }
 
             public static class Management
@@ -30,8 +30,8 @@ namespace PPRP
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "500";
-                public static DateTime LastUpdate = new DateTime(2022, 09, 20, 18, 30, 00);
+                public static string Build = "510";
+                public static DateTime LastUpdate = new DateTime(2022, 09, 20, 20, 30, 00);
             }
         }
     }
