070ed196cce42f01c983b259fc8f66c857bdc7df
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-30 20:53:14 +0700

    Election.Analytic.System - PPRP  #548 - Update projects   - MPD2562x350UnitSummary class     - Update Save method -> get error from db.     - Update Import method -> get error from db.

--- 02.Domains.and.Models/PPRP.Domains/Domains/MPD2562x350UnitSummary.cs
+++ 02.Domains.and.Models/PPRP.Domains/Domains/MPD2562x350UnitSummary.cs
@@ -146,17 +146,22 @@ namespace PPRP.Domains
             return rets;
         }
 
-        public static void Save(MPD2562x350UnitSummary value)
+        public static NDbResult Save(MPD2562x350UnitSummary value)
         {
             MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult ret = new NDbResult();
 
             IDbConnection cnn = DbServer.Instance.Db;
             if (null == cnn || !DbServer.Instance.Connected)
             {
                 string msg = "Connection is null or cannot connect to database server.";
                 med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
 
-                return;
+                return ret;
             }
 
             var p = new DynamicParameters();
@@ -173,21 +178,66 @@ namespace PPRP.Domains
             try
             {
                 cnn.Execute("SaveMPD2562x350UnitSummary", p, commandType: CommandType.StoredProcedure);
-
                 // Set error number/message
-                int errNum = p.Get<int>("@errNum");
-                string errMsg = p.Get<string>("@errMsg");
-                if (errNum != 0)
-                {
-                    Console.WriteLine(errMsg);
-                }
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
             }
             catch (Exception ex)
             {
                 med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
             }
 
-            return;
+            return ret;
+        }
+
+        public static NDbResult Import(MPD2562x350UnitSummary value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult ret = new NDbResult();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ProvinceName", value.ProvinceName);
+            p.Add("@PollingUnitNo", value.PollingUnitNo);
+            p.Add("@RightCount", value.RightCount);
+            p.Add("@ExerciseCount", value.ExerciseCount);
+            p.Add("@InvalidCount", value.InvalidCount);
+            p.Add("@NoVoteCount", value.NoVoteCount);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("ImportMPD2562x350UnitSummary", p, commandType: CommandType.StoredProcedure);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
         }
 
         #endregion
