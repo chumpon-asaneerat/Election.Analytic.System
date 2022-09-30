fc04b5e964387a3fdfba0397b67a8cd1d3ee057d
chumpon-asaneerat <chumponsenate@gmail.com>
2022-09-30 21:34:30 +0700

    Election.Analytic.System - PPRP  #557 - Update projects   - MRegion class     - Add Save method.

--- 02.Domains.and.Models/PPRP.Domains/Domains/MRegion.cs
+++ 02.Domains.and.Models/PPRP.Domains/Domains/MRegion.cs
@@ -87,6 +87,50 @@ namespace PPRP.Domains
             return rets;
         }
 
+        public static NDbResult Save(MRegion value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult ret = new NDbResult();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@RegionId", value.RegionId);
+            p.Add("@RegionName", value.RegionName);
+            p.Add("@GeoGroup", value.GeoGroup);
+            p.Add("@GeoSubGroup", value.GeoSubGroup);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveMRegion", p, commandType: CommandType.StoredProcedure);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+            return ret;
+        }
+
         #endregion
     }
 }
